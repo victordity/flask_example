55c43069b56998d3777209f807be1574588f4730
Danilo Ribeiro <danilogs@ciandt.com>
2018-03-01 11:30:57 -0300

    alteração V1

--- app/__init__.py
+++ app/__init__.py
@@ -11,7 +11,7 @@ def create_app():
 
     @app.route("/victor1")
     def zelda():
-        return "It's dangerous to go alone! Take This O---{:::::::::::::::>"
+        return "It's dangerous to go alone! Take This 8===>"
 
     @app.route("/danilogs")
     def hello_danilo():
