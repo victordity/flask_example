c8e5283887e372de4f1d39af1d726f88931e0822
Victor Hugo <victorhg@ciandt.com>
2018-03-01 14:47:08 -0800

    Adicionar mensagem mutavel

--- app/__init__.py
+++ app/__init__.py
@@ -42,8 +42,10 @@ def create_app():
     @app.route("/victordity")
     def hellovictor():
         from random import randint
+        from flask import session
         mensagens = ["Mensagem 1", "Mensagem 2", "Mensagem 3", "Mensagem 4", "Mensagem 5"]
         idx = randint(0, 4)
+        session.pop('username', None)
         print(mensagens[idx])
 
         return render_template("victor2.html", mensagem = mensagens[idx])
